7734fa3a5627602c07c8aff813847288747d52e5
Jodezamb <67130967+Jodezamb@users.noreply.github.com>
2020-09-11 12:26:39 -0500

    Pantalla de Inicio y busqueda

--- app/src/main/java/com/example/exapracamst/MainActivity.java
+++ app/src/main/java/com/example/exapracamst/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.exapracamst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText buscar;
+    Button BuscarHeroes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buscar= (EditText)findViewById(R.id.busqueda);
+
     }
+
+
+
+
+
+
 }
